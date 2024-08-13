67dd245087a87f993acd8ca914a0dcc179be0ed6
taVde981 <124488640+taVde981@users.noreply.github.com>
2024-08-13 20:09:27 +0530

    Update Program.cs

--- TypesOfConstructors/Copy Constructor/Static Constructor/Program.cs
+++ TypesOfConstructors/Copy Constructor/Static Constructor/Program.cs
@@ -1,1 +1,34 @@
+using System;
+
+class MyClass
+{
+    public static int StaticValue;
+    
+    // Static constructor
+    static MyClass()
+    {
+        Console.WriteLine("Static constructor called.");
+        StaticValue = 42;
+    }
+
+    public MyClass()
+    {
+        Console.WriteLine("Instance constructor called.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Static constructor is called only once, before the first time the class is accessed.
+        Console.WriteLine($"Static Value: {MyClass.StaticValue}");
+        
+        // Creating an instance of the class
+        MyClass obj = new MyClass();
+        
+        // The static constructor won't be called again
+        Console.WriteLine($"Static Value: {MyClass.StaticValue}");
+    }
+}
 ﻿
