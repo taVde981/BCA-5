2a4d0097e9b00ff6b7a86e785ff1433057a69941
taVde981 <124488640+taVde981@users.noreply.github.com>
2024-08-13 09:46:45 +0530

    Add files via upload

---
+++ TypesOfConstructors/Copy Constructor/Default Constructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TypesOfContructors{
+    public class Box{
+        int length, breadth, height;
+        public Box(){
+            length = 0;
+            breadth = 0;
+            height = 0;
+        }
+
+        public void show(){
+            Console.WriteLine("Default: "+length+" "+breadth+" "+height);
+        }
+    }
+
+    public class Factory{
+        public static void Main(string[] args){
+            Box box1 = new Box();
+            box1.show();
+        }
+    }
+}
