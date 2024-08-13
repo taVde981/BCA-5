cf06472c85bf2905c13f4d3554117c916a2bb042
taVde981 <124488640+taVde981@users.noreply.github.com>
2024-08-13 09:48:35 +0530

    Add files via upload

---
+++ TypesOfConstructors/Copy Constructor/Private Constructor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TypesOfConstructor{
+    public class Box{
+        int length, breadth, height;
+        private Box(){
+            length = 0;
+            breadth = 0;
+            height = 0;
+        }
+
+        public Box(int l, int b, int h){
+            length = l;
+            breadth = b;
+            height = h;
+        }
+
+        public Box(Box o){
+            length = o.length;
+            breadth = o.breadth;
+            height = o.height;
+        }
+
+        public void show(){
+            Box secretBox = new Box();
+            Console.WriteLine(secretBox.length+" "+secretBox.breadth+" "+secretBox.height);
+            Console.WriteLine(length+" "+breadth+" "+height);
+        }
+    }
+
+    public class Factory{
+        public static void Main(string[] args){
+            //Box box1 = new Box();
+            Box box2 = new Box(12,15,20);
+            Box cloneBox = new Box(box2);
+            //box1.show();
+            box2.show();
+            cloneBox.show();
+        }  
+    }
+}
